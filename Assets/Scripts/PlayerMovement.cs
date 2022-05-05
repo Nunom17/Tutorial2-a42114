7439d3eceb874698cf026e30721b2e5c9e300be0
Nuno Moreira <a42114@alunos.ipb.pt>
2022-05-05 19:19:36 +0100

    Set the Values for your Variable

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,5 +19,8 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
     }
 }
